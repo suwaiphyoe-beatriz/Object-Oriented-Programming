a85cf355dd64f0bb5ee718fe987b3b1ff76db75c
Su Wai Phyoe <beatrizisme4@gmail.com>
2025-10-20 14:18:51 +0300

    Assignment 1.2 Task 2  completed

---
+++ Assignment 1.2 Task 2/src/Triangles.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Triangles {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the length of your triangle: ");
+        double length = Double.parseDouble(scanner.nextLine());
+
+        System.out.print("Enter the height of your triangle: ");
+        double height = Double.parseDouble(scanner.nextLine());
+
+        double lengthsquared = Math.pow(length, 2);
+        double heightsquared = Math.pow(height, 2);
+        double sumOfSquares = lengthsquared + heightsquared ;
+
+
+        double hypotenuse = Math.sqrt(sumOfSquares);
+
+        System.out.printf("The length of the hypotenuse is: %.4f\n", hypotenuse);
+
+    }
+}
